628eda3ce660f73a48ec097d14f3caaa04e8ce1d
Uzaifaumar1152 <uzaifaumar.tsufsccs211152@tsuniversity.edu.ng>
2025-02-05 10:13:17 +0100

    clone

--- project.js
+++ project.js
@@ -2,3 +2,47 @@ function goToPage(page){
 
 window.location.href = "webApp.html"
 }
+
+document.querySelector(".nav1").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.querySelector(".nav11").style.display="flex";
+    document.querySelector(".nav11").style.background="rgba(244, 245, 246, 0.93)";
+    document.querySelector(".nav11").style.padding = "20px"; 
+    document.querySelector(".nav11").style.left= "60px"; 
+    document.querySelector(".nav11").style.top= "50px";
+})
+
+document.querySelector(".nav2").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.querySelector(".nav22").style.display="flex";
+    document.querySelector(".nav22").style.background="rgba(244, 245, 246, 0.93)";
+    document.querySelector(".nav22").style.padding = "20px"; 
+    document.querySelector(".nav22").style.left= "60px"; 
+    document.querySelector(".nav22").style.top= "50px";
+})
+
+document.querySelector(".nav3").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.querySelector(".nav33").style.display="flex";
+    document.querySelector(".nav33").style.background = "rgb(244, 245, 246, )";
+    document.querySelector(".nav33").style.padding = "20px"; 
+    document.querySelector(".nav33").style.left= "60px"; 
+    document.querySelector(".nav33").style.top= "50px";
+})
+
+document.querySelector(".nav4").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.querySelector(".nav44").style.display="flex";
+    document.querySelector(".nav44").style.background="rgba(244, 245, 246, 0.93)";
+    document.querySelector(".nav44").style.padding = "20px"; 
+    document.querySelector(".nav44").style.left= "60px"; 
+    document.querySelector(".nav44").style.top= "50px";
+})
+document.querySelector(".div6").addEventListener("click", (e)=>{
+    e.preventDefault();
+    document.querySelector(".div61").style.display="flex";
+    document.querySelector(".div62").style.background="rgba(244, 245, 246, 0.93)";
+    document.querySelector(".div63").style.padding = "20px"; 
+    document.querySelector(".div64").style.left= "60px"; 
+    document.querySelector(".div65").style.top= "50px";
+})
